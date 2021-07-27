810c1950c8883131a1d219d6b6518c37c8e03c5e
joblackpoc <duopoc@gmail.com>
2021-07-27 14:22:02 +0700

    modified project e-learning

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Author, Category, Course, Comment
-# Register your models here.
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Comment)
